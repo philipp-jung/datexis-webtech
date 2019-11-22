0ba3f87d24eb0502f39fb4bcad61911c2d0581ec
Philipp Jung <philippjung@posteo.de>
2019-11-22 16:01:42 +0100

    added h2 to frontend to demo state synchronicity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,6 +36,7 @@ class App extends React.Component {
           <button onClick={this.setTime}>Set time</button>
           <input id='time_setter' placeholder='Your favorite time' />
           <p>This is the website. Here is an image.</p>
+          <h2>{this.state.date}</h2>
           <img src='./forest.jpg' alt='forest' />
           <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
 
